66b7685c4971895042e66d94e6265472236869c3
Krzysztof Zaporowski <krzysztofzaporowski@gmail.com>
2025-03-27 20:19:32 +0100

    Commit v2

--- springboot_html/src/main/java/com/test/springboot_html/ErrorController.java
+++ springboot_html/src/main/java/com/test/springboot_html/ErrorController.java
@@ -1,9 +1,7 @@
 package com.test.springboot_html;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
